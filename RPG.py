0af31c213008b020dba4807b2715f561cb993be1
MR-LEFLAR <51132310+MR-LEFLAR@users.noreply.github.com>
2019-10-02 13:16:54 -0600

    Create RPG.py

---
+++ RPG.py
@@ -0,0 +1,55 @@
+# Name: Jaden Leflar
+# Period: 4
+# Course: CS30
+# Date created: 01/10/19
+# Description: Pseudocode for the RPG game
+
+"""
+DAY 1
+
+You arrive at school for the first time
+You say goodbye to your mom as you walk into the entrance
+'Bye mom, love you' - you say
+
+While walking through the school finding your classes, the bell rings
+You were unable to find any of your classes
+
+A timer starts to count down for 5 minutes
+You lose the game if you are unable to find your classes
+
+Inventory promt is given
+    - Gum
+    - School_Map
+    - Wallet
+    - Backpack
+    Inside Backpack
+        - Binder
+        - Pencil
+        - Eraser
+        - Lunch
+
+After looking at the school map, you are given directions to where to go
+such as:
+First right
+Second left
+First left
+Third right
+Fourth door on your right
+
+You are expected to memorize this
+
+Once you reach class (On time), teacher greets you and gives you a seat
+You are writing down notes, literally as a player
+- Write this down because this is going to be on the test -
+If the player cannot write it down properly, they will lose
+A timer is given
+represents the time before the board is erased
+
+Unexpected encounter occurs
+Random student starts talking to you
+'Man, isn't this school big? I could barely find my class in time.'
+     - Friendly
+     - Hostile
+     - Too Friendly
+     - Say nothing
+"""
