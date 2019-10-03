a881788d6b30592c36a3c738a82b4e1f0d876f29
MR-LEFLAR <51132310+MR-LEFLAR@users.noreply.github.com>
2019-10-03 14:02:41 -0600

    Update RPG.py

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # High_School_RPG
-A texted based adventure game in High School
+
+You must survive through high school, simple right?
+
+## Installation
+
+Python 3.7 is needed to play this game. [Python 3.7](https://www.python.org/downloads/) to install Python.
+
+
+## Run the Program
+
+```python
+python RPG.py
+```
--- RPG.py
+++ RPG.py
@@ -28,8 +28,9 @@ Inventory promt is given
         - Eraser
         - Lunch
 
-After looking at the school map, you are given directions to where to go
-such as:
+Choose School_Map
+
+After looking at the school map, you are given directions to where to go:
 First right
 Second left
 First left
@@ -38,7 +39,13 @@ Fourth door on your right
 
 You are expected to memorize this
 
-Once you reach class (On time), teacher greets you and gives you a seat
+Gives you sections of hallways where you can choose one of four options
+    a) Left
+    b) Right
+    c) Stright
+    d) Back
+
+Once you reach class (On time), the teacher greets you and gives you a seat
 You are writing down notes, literally as a player
 - Write this down because this is going to be on the test -
 If the player cannot write it down properly, they will lose
@@ -52,4 +59,162 @@ Random student starts talking to you
      - Hostile
      - Too Friendly
      - Say nothing
+
+Choose 'Friendly'
+
+You responed with
+'I know right, it's too big and nobody even showed us around'
+'Very true, my name is Jacob, what's yours?'
+FRIEND accquired
+
+Bell rings
+timer begins again
+
+Inventory promt is given
+    - Gum
+    - School_Map
+    - Wallet
+    - Backpack
+    Inside Backpack
+        - Binder
+        - Pencil
+        - Eraser
+        - Lunch
+
+Choose School_Map
+
+After looking at the school map, you are given directions to where to go:
+Four left
+Second left
+sixth right
+first left
+second door on your right
+
+You are expected to memorize this
+
+Gives you sections of hallways where you can choose one of four options
+    a) Left
+    b) Right
+    c) Stright
+    d) Back
+
+Once you reach class (On time) someone stops you before you go Inside
+Unexpected encounter occurs
+'Sup bro? Just wanted to let you know that your screwed for this class'
+     - Friendly
+     - Hostile
+     - Too Friendly
+     - Say nothing
+
+Choose 'Hostile'
+
+You respond with
+'Thank you so much for that, I really couldn't care less'
+'Hey! Nobody is allowed to talk to Roger like that'
+He walks through you, pushing your shoulder and
+Did he literally wait there to tell you that?
+ENEMY accquired
+
+You enter the classroom
+'You shouldn't take so long to get to class, your late by 10 seconds.'
+'Tisk tisk.'
+To class: 'Okay class, we are going a play a fun game called "get-to-know.'
+'All you have to do is write down somethings I should know about on paper.'
+'NO TALKING!'
+
+--Answers don't matter--
+What is your favourite colour?
+    a) Blue
+    b) Green
+    c) Red
+    d) Yellow
+
+Why did you decide to take this class?
+    a) This class is fun
+    b) I love the teacher
+    c) I had to take it
+    d) What class is this?
+
+What do you like to do for fun?
+    a) Draw
+    b) Watch TV
+    c) Hangout with friends
+    d) I don't do anything
+
+Bell rings
+
+Inventory promt is given
+    - Gum
+    - School_Map
+    - Wallet
+    - Backpack
+    Inside Backpack
+        - Binder
+        - Pencil
+        - Eraser
+        - Lunch
+
+Choose School_Map
+
+After looking at the school map, you are given directions to where to go:
+Seventh Right
+First left
+First right
+First left
+second door on your left
+
+You are expected to memorize this
+
+Gives you sections of hallways where you can choose one of four options
+    a) Left
+    b) Right
+    c) Stright
+    d) Back
+
+ You enter the classroom (on time)
+Teacher: 'Alright class, please take a seat.'
+'We are already behind so I have to give you a test'
+'Don't worry, it's just everything you've learned throughout school'
+
+You are given the test
+
+What is the slope of the given linear function?
+-6y + 6x = 12
+    a) 2
+    b) 6
+    c) -6
+    d) It doesn't have one
+
+What is the angle of D> within the triangle (Degrees)
+B> = 120 - C>
+C> = 27
+A> = B> + C> + D>
+    a) 1
+    b) 27
+    c) 180
+    d) 60
+
+Unexpected encounter occurs
+A kid who clearly doesn't know what to do is trying to talk to you
+'Pssssssst, hey man! Can you help me out?'
+'I don't know anything on this test, could you give me the answer for
+number two please?'
+    - Help_him
+    - Don't_help_him
+    - Say_nothing
+    - Say_something_mean
+
+Choose Don't help him
+You - 'Sorry man, I don't want to get in trouble.
+Plus I don't know either.'
+Him - 'It's okay man, I get it, you don't like helping people, it's cool.'
+
+What does 6C1 equal?
+    a) 3
+    b) 1
+    c) 6
+    d) 36
+
+You finish the test, passing
+CONGRATS, YOU FINISHED YOUR FIRST DAY OF SCHOOL
 """
