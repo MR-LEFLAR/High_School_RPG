2fed63c697d89026242f64ed4ab808087631e72c
MR-LEFLAR <jaden.leflar@rbe.sk.ca>
2019-10-29 14:07:58 -0600

    Update RPG_Nested_Dictionaries.py

--- RPG_Nested_Dictionaries.py
+++ RPG_Nested_Dictionaries.py
@@ -4,24 +4,28 @@
 # Date created: 25/10/19
 # Description: RPG Nested Dictionaries
 
+
+def thingy(Name):
+    # prints out the items, keys, and values in the Dictionaries
+    for Name, key in Name.items():
+        # Does a loop through the dictionaies and prints all their contents
+        print(Name + ":")
+        for items in key:
+            # Goes through the nested dictionaries and prints their contents
+            print(f"\t{items} - {key[items]}")
+
 # who is the main character and what are his traits
 main_character = {
-        # He does not have a name because it is the player
+        "Main character": {
         "Name": "*@#$!&%^/",
         "Age": 14,
         "Grade": 9,
-        "Personality": "depends on player choices"
+        "Personality": "depends on player choices"}
 }
-
-#Prints the title for the list
-print("MAIN CHARACTER")
-for main in main_character.keys():
-    print(f"{main}:")
-# Makes it easier to read
-print("---------------------------------------------------------------------")
 
 # All the other characters shown in the game
 side_characters = {
+        "Side characters": {
         # In order of apperence
         "mom": "mother of main character",
         "period 1 teacher": "English teacher, Mrs. C",
@@ -29,39 +33,47 @@ side_characters = {
         "Roger": "school bully, will always be an enemy",
         "period 2 teacher": "strict science teacher, Mr. Hateskids",
         "period 3 teacher": "Fast paste math teacher, Mrs. O",
-        "Kate": "The girl who cheats on tests, can be friend or enemy"
+        "Kate": "The girl who cheats on tests, can be friend or enemy"}
 }
-
-#Prints the title for the list
-print("SIDE CHARACTER")
-for side in side_characters.keys():
-    print(side)
 
 # Various locations within the school and the school itself
 locations = {
+        "Locations": {
         "School": "Only location in the game, where the player roams",
         "Period 1": "English class, may have to read or write notes",
         "Period 2": "Science class, taking tests and doing experiments",
-        "Period 3": "Math class, lots of tests with real math questions",
+        "Period 3": "Math class, lots of tests with real math questions"}
 }
-
-# Printing the locations
-print(locations)
-# Makes it easier to read
-print("--------------------------------------------------------------")
 
 # Inventory prompt that the player can choose what they want
 Inventory = {
-        "Gum": "Can be eaten or given out, people like gum",
-        "School_Map": "Used to navigate your way through the school",
-        "Wallet": "Shows the money you have, can buy snacks or give out",
+        "Gum": {"Total pieces left": 10},
+        "Wallet": {"Total cash": "$12.27"},
         "Backpack": {"Binder": "Used to refer to notes for tests",
         "Pencil": "Used to write tests and notes, can have more than one",
         "Eraser": "Used to erase tests and notes, can have more than one",
         "Lunch": "Can be eaten at any time"}
-}
+            }
 
-# Printing the Inventory
-print(Inventory)
+# Makes it easier to read
+print("------------------------------------------------------------------")
+# Prints out the main title for the dictionary
+print("THE MAIN CHARACTER")
+thingy(main_character)
+# Makes it easier to read
+print("------------------------------------------------------------------")
+# Prints out the main title for the dictionary
+print("THE SIDE CHARACTERS")
+thingy(side_characters)
+# Makes it easier to read
+print("------------------------------------------------------------------")
+# Prints out the main title for the dictionary
+print("THE LOCATIONS")
+thingy(locations)
+# Makes it easier to read
+print("------------------------------------------------------------------")
+# Prints out the main title for the dictionary
+print("THE INVENTORY")
+thingy(Inventory)
 # Makes it easier to read
 print("------------------------------------------------------------------")
