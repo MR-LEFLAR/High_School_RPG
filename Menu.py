20bc43d40b30e7a24a1d63159d18659549dea2c8
MR-LEFLAR <jaden.leflar@rbe.sk.ca>
2019-11-01 14:08:23 -0600

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -4,48 +4,64 @@
 # Date created: 29/10/19
 # Description: RPG Simple Menu start up
 
+# Variable that decide which menu will pop up
 starting_up = 1
 a = "not_null"
 menu = 0
+b = "idk"
 
 if starting_up == 1:
-    print("---------------------WELCOME TO VIRTUAL HELL---------------------")
-    print("-------------------------AKA High School-------------------------")
+    # Shows the titles
+    print("------------------------WELCOME TO VIRTUAL HELL-------------------")
+    print("----------------------------AKA High School-----------------------")
+    # Creates a book out of characters on the keyboard
     print("""
                               ______ ______
                             _/      Y      \_
                            // ~~ ~~ | ~~ ~  \\
                           // ~ ~ ~~ | ~~~ ~~ \\
                          //________.|.________\\
-                        `----------`-'----------' """)
+                        '----------`-'----------'
+        """)
     starting_up = 0
     a = "null"
 
-if a == "null":
+while a == "null":
+    # Letting the player actually interact with the game
     print("Type 'start' to begin")
-    a = input()
-    if a == "start":
+    b = input()
+    if b == "start":
+        # Shows the instructions
         print("\t----Here are the instructions----")
         print("\tVarious stages of the game have different controls")
         print("\tYou will be told what you can and cannot type\n")
         print("\tType 'quit' at any point to leave the game")
         print("\ttype 'lets do this' to begin")
-        menu = input()
+        a = "not_null_again"
+    elif b == "quit":
+        # Shows that you can quit at any time
+        print("Thanks for playing!")
+        a = "not_null"
+    elif b != "start" or "quit":
+        # Tells the player to insert a valid command
+        print("Invalid command")
+        print("Type 'start' to begin")
+        a = "null"
 
-if a != "null" or "start":
-    print("Invalid command")
-    print("Type 'start' to begin")
-    a = input()
-
-if menu == "lets do this":
-    print("=================================================================")
-    print("----------------------------- DAY 1 -----------------------------")
-    print("=================================================================")
-if menu != "Lets do this" or "quit":
-    print("Invalid command")
-    print("Type 'lets do this' to begin")
-    print("Type 'quit' to leave the game")
+while a == "not_null_again":
     menu = input()
-
-if menu == "quit":
-    print("Thanks for playing!")
+    if menu == "lets do this":
+        # Start of day one
+        print("=============================================================")
+        print("--------------------------- DAY 1 ---------------------------")
+        print("=============================================================")
+        a = "not_null"
+    elif menu == "quit":
+        # Shows that you can quit at any time
+        print("Thanks for playing!")
+        a = "not_null"
+    elif menu != "lets do this" or "quit":
+        # Tells the player to insert a valid command
+        print("Invalid command")
+        print("Type 'lets do this' to begin")
+        print("Type 'quit' to leave the game")
